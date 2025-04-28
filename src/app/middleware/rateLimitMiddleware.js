13134934c984917727003a0cd55cf40949bc7a6f
Nhữ Hoàng Hải <nhuhoanghai1900@gmail.com>
2025-04-28 16:32:18 +0700

    Update rate limit configuration to reduce maximum requests

--- src/app/middleware/rateLimitMiddleware.js
+++ src/app/middleware/rateLimitMiddleware.js
@@ -1,9 +1,9 @@
-import rateLimit from "express-rate-limit";
+import rateLimit from "express-rate-limit"
 
 const restoreRateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 phút
-    max: 2000, // Tối đa 10 lần gọi trong 15 phút
-    message: "Quá nhiều yêu cầu. Vui lòng thử lại sau.",
+  windowMs: 15 * 60 * 1000, // 15 phút
+  max: 1000, // Tối đa 10 lần gọi trong 15 phút
+  message: "Quá nhiều yêu cầu. Vui lòng thử lại sau.",
 })
 
 export default restoreRateLimiter
