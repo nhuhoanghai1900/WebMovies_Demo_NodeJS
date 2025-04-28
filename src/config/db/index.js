d3f1e1d20deee156d09462464688f156372183c2
Nhữ Hoàng Hải <nhuhoanghai1900@gmail.com>
2025-04-28 17:58:39 +0700

    Update MongoDB connection logic to use environment variable for URI and improve error handling

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,10 +1,29 @@
 import mongoose from "mongoose"
 
-async function connect() {}
-try {
-  await mongoose.connect("mongodb://127.0.0.1:27017/education_dev")
-  console.log("Connect success MongoDb!!!")
-} catch (error) {
-  console.log("Connect failure MongoDb!!!")
+// async function connect() {}
+// try {
+//   await mongoose.connect("mongodb://127.0.0.1:27017/education_dev")
+//   console.log("Connect success MongoDb!!!")
+// } catch (error) {
+//   console.log("Connect failure MongoDb!!!")
+// }
+// export default connect
+
+// import mongoose from "mongoose"
+
+async function connect() {
+  try {
+    const mongoUri = process.env.MONGODB_URI
+
+    if (!mongoUri) {
+      console.error("MongoDB URI is not defined!")
+      return
+    }
+    await mongoose.connect(mongoUri)
+    console.log("Connect success MongoDB!!!")
+  } catch (error) {
+    console.log("Connect failure MongoDB!!!")
+    console.error(error)
+  }
 }
 export default connect
